6f1c2d30790b125dde17a51ebb604a331c32aea4
KeremUZUN <kerembh4@gmail.com>
2019-06-14 20:11:39 +0300

    Proje dosyalarım OnlineRepository'me yüklenme hazır

---
+++ And.Eticaret.UI.WEB/Controllers/Base/AndControllerBase.cs
@@ -0,0 +1,29 @@
+﻿using And.Eticaret.Core.Model.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+
+namespace And.Eticaret.UI.WEB.Controllers.Base
+{
+    public class AndControllerBase : Controller
+    {
+        // Kullanıcı Login Kontrolü
+        public bool IsLogin { get; private set; }
+
+
+        // Giriş yapmış küşünün Id si
+        public int LoginUserId { get; set; }
+
+        // Giriş yapan kullanıcının bilgileri
+        public User LoginUserEntity { get; private set; }
+        protected override void Initialize(RequestContext requestContext)
+        {   
+            //Todo : Üye Giriş işlemleri sonrası değişecek
+            
+            base.Initialize(requestContext);
+        }
+    }
+}
---
+++ And.Eticaret.UI.WEB/Controllers/ProductController.cs
@@ -0,0 +1,22 @@
+﻿using And.Eticaret.Core.Model;
+using And.Eticaret.UI.WEB.Controllers.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace And.Eticaret.UI.WEB.Controllers
+{
+    public class ProductController : AndControllerBase
+    {
+        AndDB db = new AndDB();
+        // GET: Product
+        [Route("Urun/{title}/{id}")]
+        public ActionResult Detail(string title,int id)
+        {
+            var prod = db.Products.Where(x => x.ID == id).FirstOrDefault();
+            return View(prod);
+        }
+    }
+}
