dfb692c5b03f1cf55dea0d084a7cc7b2c97df685
KeremUZUN <kerembh4@gmail.com>
2019-06-17 15:19:22 +0300

    feature 3  siparis

--- And.Eticaret.UI.WEB/Controllers/BasketController.cs
+++ And.Eticaret.UI.WEB/Controllers/BasketController.cs
@@ -26,11 +26,20 @@ namespace And.Eticaret.UI.WEB.Controllers
             var rt = db.SaveChanges();
             return Json(rt,JsonRequestBehavior.AllowGet);
         }
+
+        [Route("Sepetim")]
         public ActionResult Index()
         {
-
+            var data = db.Baskets.Include("Product").Where(x => x.UserID == LoginUserID).ToList();
            
-            return View();
+            return View(data);
+        }
+        public ActionResult Delete(int id)
+        {
+            var deleteItem = db.Baskets.Where(x => x.ID == id).FirstOrDefault();
+            db.Baskets.Remove(deleteItem);
+            db.SaveChanges();
+            return RedirectToAction("Index");
         }
     }
 }
